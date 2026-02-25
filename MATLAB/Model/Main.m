ebaa7f3ffc926ad3d678090cd1a2ec5a3defbe55
Васильев Андрей Сергеевич <vasilevs2003@yandex.ru>
2026-02-25 11:18:22 +0300

    Добавлено отслеживание символьной синхронизации каждые 100 периодов C/A кода. Без накопления

--- MATLAB/Model/Main.m
+++ MATLAB/Model/Main.m
@@ -5,11 +5,18 @@ close all;
     % Опорная частота дискретизации
         Fbase = 1.023e6;
     % Коэффициент передискретизации
-        sps = 6;
+        sps = 2;
     % Длина C/A кода в чипах
         CACodeLen = 1023;
     % Число периодов C/A кода, исп-мых при обнаружении
         NumCACodePers = 20;
+
+    % Число периодов C/A кодов, после которых необходимо подстраивать
+    % синхронизацию по времени
+        NumCA2Sync = 100;
+
+    % Прорисовка результатов
+        isDrawRes = false;
 
 % Вычисляемые параметры
 
@@ -22,15 +29,18 @@ close all;
     File.Fs0      = Fbase * 2;
     File.dF       = 0;
     File.FsDown   = 1;
-    File.FsUp     = 3;
+    File.FsUp     = 1;
     NumOfShiftedSamples = 0;
-    NumOfNeededSamples  = (NumCACodePers+1) * CACodeLen * sps - 1;
+    NumOfNeededSamples  = 4 * File.Fs0 * File.FsUp;
 
     [Signal, File] = ReadSignalFromFile(File, NumOfShiftedSamples, ...
         NumOfNeededSamples);
 
 % Обнаружение спутника
     CACodeNum = 1;
+
+    % Отрезок сигнала в начале записи
+        SignalShort = Signal(1:(NumCACodePers+1) * CACodeLen * sps - 1);
 
     % Массив значений сдвигов частоты
         FVals = 0 : 400 : 5200;
@@ -49,12 +59,78 @@ close all;
                     exp(1j*2*pi*FVals(k) * (0:length(refSeq)-1) / File.Fs);
 
             % Корреляция
-                buf  = conv(Signal, fliplr(conj(refSeq) ), "valid");
+                buf  = conv(SignalShort, fliplr(conj(refSeq) ), "valid");
 
             % Некогерентное накопление результата
                 buf1 = reshape(buf, CACodeLen * sps, [] ).';
                 CorrVals(k, :) = sum(abs(buf1) );
         end
 
-        figure(CACodeNum)
-        surf(CorrVals)
+        if isDrawRes
+            figure(CACodeNum)
+            surf(CorrVals)
+        end
+
+    % Определение грубого сдвига частоты и сдвига до начала первого C/A
+    % кода
+        [buf, IndsMaxY] = max(CorrVals);
+        [~, IndMaxX]    = max(buf);
+        IndMaxY = IndsMaxY(IndMaxX);
+
+        df = FVals(IndMaxY);
+        Offset = IndMaxX - 1;
+
+% Грубая подстройка частоты
+    Signal = Signal .* exp(-1j*2*pi * df * (0:length(Signal)-1)/File.Fs);
+
+% Вычисление корреляций C/A кодов с опорной последовательностью
+    % Число полных C/A кодов в записи
+        NumFullCACodes = floor( (length(Signal) - Offset) / length(refSeq) );
+
+    % Указатель, на начало очередного периода C/A кода
+        Ptr = Offset +1;
+
+    % Значения корреляций
+        CorrValsDemod = zeros(NumFullCACodes, 1);
+
+    % Опорная последовательность
+        refSeq = repelem(ethCACode, sps).';
+
+    % Счётчик периодов C/A кодов, обработанных после последней подстройки
+    % синхронизации по времени
+        CntrCA = 0;
+
+    for k = 1 : NumFullCACodes
+        % Обнуление переменной, отвечающей за подстройку символьной
+        % синхронизации
+            Ofst = 0;
+
+        % Вычисление корреляции. При необходимости выполнение
+        % Early-Late-Prompt корреляций и подстройка синхронизации по
+        % времени
+            if CntrCA == NumCA2Sync
+                % Обнуление счётчика
+                    CntrCA = 0;
+
+                % Early-Prompt-Late корреляции
+                    CorrValsEPL = zeros(1, 3);
+
+                    CorrValsEPL(1) = Signal( (0:length(refSeq)-1) + Ptr - 1) * refSeq;
+                    CorrValsEPL(2) = Signal( (0:length(refSeq)-1) + Ptr) * refSeq;
+                    CorrValsEPL(3) = Signal( (0:length(refSeq)-1) + Ptr + 1) * refSeq;
+
+                % Выбор наибольшего значения
+                    [CorrValsDemod(k), Ofst] = max(abs(CorrValsEPL) );
+
+                Ofst = Ofst - 2;
+
+            else
+                CorrValsDemod(k) = Signal( (0:length(refSeq)-1) + Ptr) * refSeq;
+            end
+
+        % Инкремент счётчика
+            CntrCA = CntrCA + 1;
+
+        % Обновление значение указателя
+            Ptr = Ptr + length(refSeq) + Ofst;
+    end
