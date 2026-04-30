0dd15b55239e1c2b853f2ed32772adc6467ff27e
ans.vasilev <vasilevs2003@yandex.ru>
2026-04-30 12:50:21 +0300

    feat: реализована P73_RenewSatPos.m

--- MATLAB/Model/P73_RenewSatPos.m
+++ MATLAB/Model/P73_RenewSatPos.m
@@ -10,5 +10,22 @@ function OutSatPos = P73_RenewSatPos(SatPos, TProp, Params) %#ok<INUSD>
 % Выходные переменные
 %   OutSatPos - массив (3x1) скорректированных координат спутника.
 
-% WGS 84 value of the earth's rotation rate (rad/sec)
-    % dOmega_e = 7.2921151467e-5;
+%% ПЕРЕСЧЁТ КООРДИНАТ СПУТНИКА
+% Извлечение параметров
+    xs_k = SatPos(4);
+    ys_k = SatPos(5);
+    i_k  = SatPos(6);
+    Omega_k  = SatPos(7);
+    dOmega_e = SatPos(8);
+
+% Поправка долготы восходящего узла
+    Omega_k_new = Omega_k - dOmega_e * TProp;
+
+% Вычисление координат
+    x_k = xs_k*cos(Omega_k_new) - ys_k*cos(i_k)*sin(Omega_k_new);
+    y_k = xs_k*sin(Omega_k_new) + ys_k*cos(i_k)*cos(Omega_k_new);
+    z_k = ys_k*sin(i_k);
+
+% Результат
+    OutSatPos = [x_k; y_k; z_k];
+end
