f46e842c6978ad1f9811dff23d45ece40f51d47e
ans.vasilev <vasilevs2003@yandex.ru>
2026-05-12 19:02:47 +0300

    feat: реализована P74_Cartesian2Spherical.m

--- MATLAB/Model/P74_Cartesian2Spherical.m
+++ MATLAB/Model/P74_Cartesian2Spherical.m
@@ -3,7 +3,8 @@ function [Lat, Lon, Alt] = P74_Cartesian2Spherical(xyz, Params)
 % координат в сферическую систему координат
 %
 % Входные параметры
-%   x, y, z - координаты в прямоугольной системе координат в метрах.
+%   xyz - массив значений x, y, z - координат в прямоугольной системе 
+%     координат в метрах.
 %
 % Выходные параметры
 %   Latitude, Longitude, Altitude - широта и долгота в радианах, высота в
@@ -37,3 +38,78 @@ function [Lat, Lon, Alt] = P74_Cartesian2Spherical(xyz, Params)
             case 2
                 a = 6378245; % Красовский - 1942
         end
+
+    % Координаты в прямоугольной системе
+        x = xyz(1);
+        y = xyz(2);
+        z = xyz(3);
+
+ % Вычисление долготы
+    Lon = atan2(y, x);
+    
+% Вычисление широты и высоты в зависимости от выбранного алгоритма
+    n = size(x, 1);
+    Lat = zeros(n, 1);
+    Alt = zeros(n, 1);
+    
+% Квадрат эксцентриситета
+    e2 = 2*ell - ell^2;
+
+if AlgType == 0
+% Алгоритм по стандарту РФ (итерационный)
+    for i = 1:n
+        % Начальное приближение широты
+            p = sqrt(x(i)^2 + y(i)^2);
+            Lat0 = atan2(z(i), p * (1 - e2));
+        % Итерационный процесс
+        for iter = 1:10
+            N = a / sqrt(1 - e2 * sin(Lat0)^2);
+            h = p / cos(Lat0) - N;
+            Lat_new = atan2(z(i), p * (1 - e2 * N / (N + h)));
+            
+            if abs(Lat_new - Lat0) < 1e-12
+                Lat0 = Lat_new;
+                break;
+            end
+            Lat0 = Lat_new;
+        end
+        
+        Lat(i) = Lat0;
+        N = a / sqrt(1 - e2 * sin(Lat(i))^2);
+        Alt(i) = p / cos(Lat(i)) - N;
+    end
+    
+else
+% Алгоритм по книге (метод Боуринга - прямой без итераций)
+    for i = 1:n
+        p = sqrt(x(i)^2 + y(i)^2);
+        
+        % Вычисление геодезической широты
+            r = sqrt(p^2 + z(i)^2);
+            u = atan2(z(i) * (1 - ell), p);
+        
+        % Вспомогательные величины
+            sin_u = sin(u);
+            cos_u = cos(u);
+            
+            Lat0 = atan2(z(i) + e2 * a * sin_u^3, ...
+                        p - e2 * a * cos_u^3);
+        
+        % Уточнение для лучшей точности (один-два шага)
+        for iter = 1:3
+            N = a / sqrt(1 - e2 * sin(Lat0)^2);
+            h = p / cos(Lat0) - N;
+            Lat_new = atan2(z(i), p * (1 - e2 * N / (N + h)));
+            
+            if abs(Lat_new - Lat0) < 1e-12
+                Lat0 = Lat_new;
+                break;
+            end
+            Lat0 = Lat_new;
+        end
+        
+        Lat(i) = Lat0;
+        N = a / sqrt(1 - e2 * sin(Lat(i))^2);
+        Alt(i) = p / cos(Lat(i)) - N;
+    end
+end
